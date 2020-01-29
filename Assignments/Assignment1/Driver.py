9c9ff1b84582eabe31203ad4b131b9faef7360c3
Kevin Chung <chungk90@hotmail.com>
2020-01-28 19:06:34 -0800

    Created register_user() and run() methods

--- Assignments/Assignment1/Driver.py
+++ Assignments/Assignment1/Driver.py
@@ -1,11 +1,42 @@
+from Assignments.Assignment1.user import User
+from Assignments.Assignment1.bank_account import BankAccount
+
+
 class Driver:
 
     def __init__(self):
         pass
 
+    @classmethod
+    def register_user(cls):
+        input_name = input("Enter the user's name: ")
+        input_age = int(input("Enter the user's age: "))
+        input_bank_name = input("Enter the user's bank name: ")
+        input_account_num = int(input("Enter the user's "
+                                      "bank account number: "))
+        input_bank_balance = float(input("Enter the user's bank balance: "))
+
+        bank_account = BankAccount(input_bank_name, input_account_num,
+                                   input_bank_balance)
+        user = User(input_name, input_age, bank_account)
+
+        return user
+
+    @classmethod
+    def run(cls):
+        print("Please select an option:")
+        input_option = int(input("1) Register a new user\n"
+                                 "2) Load test users\n"))
+
+        if input_option == 1:
+            cls.register_user()
+        elif input_option == 2:
+            User.load_test_users()
+
 
 def main():
-    pass
+    driver = Driver()
+    driver.run()
 
 
 if __name__ == '__main__':
--- Assignments/Assignment1/budget.py
+++ Assignments/Assignment1/budget.py
@@ -5,11 +5,11 @@ class Budget:
 
     def __init__(self, total_budget, budget_type, amount_spent, amount_left,
                  is_locked=False):
-        self.is_locked = is_locked
         self.total_budget = total_budget
         self.amount_spent = amount_spent
         self.amount_left = amount_left
         self.budget_type = budget_type
+        self.is_locked = is_locked
 
 
 class BudgetTypes(enum.Enum):
