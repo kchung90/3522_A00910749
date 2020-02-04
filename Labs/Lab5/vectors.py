a7b5e0c4f3317ad461ff4a70d2771237609a81e5
Kevin Chung <chungk90@hotmail.com>
2020-02-04 14:37:45 -0800

    updated main()

--- Labs/Lab5/vectors.py
+++ Labs/Lab5/vectors.py
@@ -101,19 +101,35 @@ class Vector:
 
 
 def main():
-    my_vector = Vector(2, 2, 2)
-    # print(my_vector)
-    # print(my_vector["x"])
-    # my_vector["x"] = 50
-    print(my_vector)
+    my_vector = Vector(1, 3, 7)
     other_vector = Vector(1, 2, 3)
-    print(other_vector)
+    print(f"First vector: {my_vector}")
+    print(f"Second vector: {other_vector}")
+    print(f"x-value of first vector: {my_vector['x']}")
+    my_vector['x'] = 5
+    print(f"Update x-value of first vector to 5: {my_vector}")
 
-    print(my_vector + other_vector)
-    print(my_vector - other_vector)
-    print(my_vector * other_vector)
-    print(my_vector * 3)
-    print(2 * my_vector)
+    print(f"\nAdd 2 vectors: {my_vector + other_vector}")
+    print(f"Subtract second vector from first vector: "
+          f"{my_vector - other_vector}")
+    print(f"Multiply 2 vectors: {my_vector * other_vector}")
+    print(f"Multiply the first vector with 3: {my_vector * 3}")
+    print(f"Multiply the second vector with 2: {2 * other_vector}")
+    print(f"Magnitude of the first vector: {abs(my_vector)}")
+    print(f"Magnitude of the second vector: {abs(other_vector)}")
+
+    print(f"\nIs the first vector less than the second vector? "
+          f"{my_vector < other_vector}")
+    print(f"Is the first vector less than or equal to the second vector? "
+          f"{my_vector <= other_vector}")
+    print(f"Is the first vector greater than the second vector? "
+          f"{my_vector > other_vector}")
+    print(f"Is the first vector greater than or equal to the second vector? "
+          f"{my_vector >= other_vector}")
+    print(f"Is the first vector equal to the second vector? "
+          f"{my_vector == other_vector}")
+    print(f"Is the first vector not equal to the second vector? "
+          f"{my_vector != other_vector}")
 
 
 if __name__ == '__main__':
