c53a5a2e48d3adbe826f7490ccee926a97fdae4d
Kevin Chung <chungk90@hotmail.com>
2020-02-04 14:20:54 -0800

    arithmetic operators functions created

--- Labs/Lab5/vectors.py
+++ Labs/Lab5/vectors.py
@@ -1,3 +1,6 @@
+import math
+
+
 class Vector:
 
     def __init__(self, x, y, z):
@@ -6,7 +9,8 @@ class Vector:
                        "z": z}
 
     def __str__(self):
-        return f"{self.vector['x']}, {self.vector['y']}, {self.vector['z']}"
+        return f"Vector: ({self.vector['x']}, {self.vector['y']}," \
+               f" {self.vector['z']})"
 
     def __getitem__(self, item):
         return self.vector[item]
@@ -14,13 +18,61 @@ class Vector:
     def __setitem__(self, key, value):
         self.vector[key] = value
 
+    def __add__(self, other):
+        result = Vector((self.vector['x'] + other.vector['x']),
+                        (self.vector['y'] + other.vector['y']),
+                        (self.vector['z'] + other.vector['z']))
+        return result
+
+    def __sub__(self, other):
+        result = Vector((self.vector['x'] - other.vector['x']),
+                        (self.vector['y'] - other.vector['y']),
+                        (self.vector['z'] - other.vector['z']))
+        return result
+
+    def __mul__(self, other):
+        if type(other) == Vector:
+            result = Vector(self.vector['y'] * other.vector['z'] -
+                            self.vector['z'] * other.vector['y'],
+                            self.vector['z'] * other.vector['x'] -
+                            self.vector['x'] * other.vector['z'],
+                            self.vector['x'] * other.vector['y'] -
+                            self.vector['y'] * other.vector['x'])
+        else:
+            result = Vector(self.vector['x'] * other,
+                            self.vector['y'] * other,
+                            self.vector['z'] * other)
+        return result
+
+    def __rmul__(self, other):
+        if type(other) == Vector:
+            result = Vector(self.vector['y'] * other.vector['z'] -
+                            self.vector['z'] * other.vector['y'],
+                            self.vector['z'] * other.vector['x'] -
+                            self.vector['x'] * other.vector['z'],
+                            self.vector['x'] * other.vector['y'] -
+                            self.vector['y'] * other.vector['x'])
+        else:
+            result = Vector(self.vector['x'] * other,
+                            self.vector['y'] * other,
+                            self.vector['z'] * other)
+        return result
+
 
 def main():
-    my_vector = Vector(10, 20, 30)
+    my_vector = Vector(2, 2, 2)
+    # print(my_vector)
+    # print(my_vector["x"])
+    # my_vector["x"] = 50
     print(my_vector)
-    print(my_vector["x"])
-    my_vector["x"] = 50
-    print(my_vector)
+    other_vector = Vector(1, 2, 3)
+    print(other_vector)
+
+    print(my_vector + other_vector)
+    print(my_vector - other_vector)
+    print(my_vector * other_vector)
+    print(my_vector * 3)
+    print(2 * my_vector)
 
 
 if __name__ == '__main__':
