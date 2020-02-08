4b6766202bcab5983492c33a3149da3ad6d95374
Kevin Chung <chungk90@hotmail.com>
2020-02-08 01:07:56 -0800

    modified the visibility of attributes for transaction

--- Assignments/Assignment1/transaction.py
+++ Assignments/Assignment1/transaction.py
@@ -19,10 +19,42 @@ class Transaction:
         :param budget_type: category of the budget as a String
         :param shop_name: name of the shop as a String
         """
-        self.amount = amount
-        self.budget_type = budget_type
-        self.shop_name = shop_name
-        self.timestamp = datetime.today().replace(microsecond=0)
+        self._amount = amount
+        self._budget_type = budget_type
+        self._shop_name = shop_name
+        self._timestamp = datetime.today().replace(microsecond=0)
+
+    @property
+    def amount(self):
+        """
+        Return the amount spent
+        :return: amount as a float
+        """
+        return self._amount
+
+    @property
+    def budget_type(self):
+        """
+        Return the budget type used for the transaction
+        :return: budget type as an integer
+        """
+        return self._budget_type
+
+    @property
+    def shop_name(self):
+        """
+        Return the name of the shop where the transaction was made
+        :return: name of the shop as a String
+        """
+        return self._shop_name
+
+    @property
+    def timestamp(self):
+        """
+        Return the time when the transaction was processed
+        :return: timestamp as a Datetime
+        """
+        return self._timestamp
 
     def __str__(self):
         """
