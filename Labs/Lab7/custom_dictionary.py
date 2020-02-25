9fa9db024856ae72f81f70c799bb994390351bdc
Kevin Chung <chungk90@hotmail.com>
2020-02-25 13:27:24 -0800

    read text file implemented

--- Labs/Lab7/custom_dictionary.py
+++ Labs/Lab7/custom_dictionary.py
@@ -1,17 +1,30 @@
 class CustomDictionary:
 
     def __init__(self, path):
-        pass
+        with open(path, mode="r", encoding="utf-8") as my_file:
+
+            lines = [line.split("\n") for line in
+                     list(my_file.read().split("--"))]
+
+            self.definitions = {value[0]: value[1:] for value in lines
+                                if value != ['']}
+
+            # self.words_queried
 
     def query(self, word):
-        pass
+        word_list = ""
+        if word in self.definitions.keys():
+            for definition in self.definitions[word]:
+                word_list += definition
+        return word_list
 
     def export(self):
         pass
 
 
 def main():
-    pass
+    test = CustomDictionary("data.txt")
+    print(test.query("rain"))
 
 
 if __name__ == '__main__':
