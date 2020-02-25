2fb58ad098fe824ec8986073a65685e8ab49b819
Kevin Chung <chungk90@hotmail.com>
2020-02-25 10:27:10 -0800

    initial commit

---
+++ Labs/Lab7/custom_dictionary.py
@@ -0,0 +1,18 @@
+class CustomDictionary:
+
+    def __init__(self, path):
+        pass
+
+    def query(self, word):
+        pass
+
+    def export(self):
+        pass
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
