66ad7f4f227e401ce17c02e87011ec1eabfde894
Kevin Chung <chungk90@hotmail.com>
2020-03-19 01:00:30 -0700

    Initial Commit

---
+++ Labs/Lab9/observers.py
@@ -0,0 +1,45 @@
+import abc
+
+
+class Observers:
+
+    def __call__(self, title, data, labels, output_name):
+        self.title = title
+        self.data = []
+        self.labels = []
+        self.output_name = output_name
+
+
+class Graph(abc.ABC):
+
+    @abc.abstractmethod
+    def __call__(self, title, data, labels, output_name):
+        pass
+
+
+class LineGraph(Graph):
+
+    def __init__(self, line_style, has_fill, fill_colour):
+        self.line_style = line_style
+        self.has_fill = has_fill
+        self.fill_colour = fill_colour
+
+    def __call__(self, title, data, labels, output_name):
+        self.title = title
+        self.data = []
+        self.labels = []
+        self.output_name = output_name
+
+
+class BarGraph(Graph):
+
+    def __init__(self, is_horizontal, edge_colour, bar_colour):
+        self.is_horizontal = is_horizontal
+        self.edge_colour = edge_colour
+        self.bar_colour = bar_colour
+
+    def __call__(self, title, data, labels, output_name):
+        self.title = title
+        self.data = []
+        self.labels = []
+        self.output_name = output_name
