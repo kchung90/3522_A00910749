f449a78dd57a49872174a421f3301aaa3bba853c
Kevin Chung <chungk90@hotmail.com>
2020-03-25 00:37:51 -0700

    AccessLevelError class created

--- Labs/Lab10/movie_database_proxy.py
+++ Labs/Lab10/movie_database_proxy.py
@@ -7,6 +7,11 @@ class UserAccessEnum(Enum):
     ADMIN = 1
 
 
+class AccessLevelError(Exception):
+    def __init__(self, task):
+        super().__init__(f"MEMBER access level is not allowed to {task}")
+
+
 class MovieDatabaseProxy:
 
     def __init__(self, access_level, db_file_name: str, movies: list = None):
@@ -21,7 +26,10 @@ class MovieDatabaseProxy:
         self.movie_db.close_connection()
 
     def insert(self, movie: Movie):
-        pass
+        if self.access_level == UserAccessEnum.ADMIN:
+            self.movie_db.insert(movie)
+        else:
+            raise AccessLevelError("insert")
 
     def view(self) -> list:
         movie_list = ["--- Movies in Cache:"]
@@ -62,13 +70,13 @@ class MovieDatabaseProxy:
 
 
 def main():
-    movie_list = [Movie("title1", "director1", 2020, "ENG"),
-                  Movie("title2", "director2", 2019, "FR"),
-                  Movie("title3", "director3", 2018, "ENG"),
-                  Movie("title4", "director1", 2020, "KR"),
-                  Movie("title5", "director2", 2018, "FR")]
-    # movie_list = []
-    proxy1 = MovieDatabaseProxy(0, "movie.db", movie_list)
+    # movie_list = [Movie("title1", "director1", 2020, "ENG"),
+    #               Movie("title2", "director2", 2019, "FR"),
+    #               Movie("title3", "director3", 2018, "ENG"),
+    #               Movie("title4", "director1", 2020, "KR"),
+    #               Movie("title5", "director2", 2018, "FR")]
+    movie_list = []
+    proxy1 = MovieDatabaseProxy(UserAccessEnum.MEMBER, "movie.db", movie_list)
     for movie in proxy1.search("", "", "FR"):
         print(movie)
 
@@ -81,6 +89,14 @@ def main():
     # print("\n\n\n cache list")
     # for a in proxy1.cache:
     #     print(a)
+    for movie in proxy1.view():
+        print(movie)
+
+    try:
+        proxy1.insert(Movie("test", "test_director", 2020, "ENG"))
+    except AccessLevelError as e:
+        print(e)
+
     for movie in proxy1.view():
         print(movie)
 
