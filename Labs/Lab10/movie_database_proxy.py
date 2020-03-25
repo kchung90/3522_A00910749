28e1b24436de03290d331e2b42b5236da6b32750
Kevin Chung <chungk90@hotmail.com>
2020-03-24 20:58:47 -0700

    UserAccessEnum class created

--- Labs/Lab10/movie_database_proxy.py
+++ Labs/Lab10/movie_database_proxy.py
@@ -1,4 +1,33 @@
+from Labs.Lab10.movie_database import Movie, MovieDatabase
+from enum import Enum
+
+
+class UserAccessEnum(Enum):
+    MEMBER = 0
+    ADMIN = 1
+
+
 class MovieDatabaseProxy:
 
-    def __init__(self):
+    def __init__(self, access_level, db_file_name: str, movies: list = None):
+        self.movie_db = MovieDatabase(db_file_name, movies)
+        self.access_level = access_level
+        self.cache = []
+
+    def connect(self):
+        pass
+
+    def close_connection(self):
+        pass
+
+    def insert(self, movie: Movie):
+        pass
+
+    def view(self) -> list:
+        pass
+
+    def delete(self, movie_id):
+        pass
+
+    def search(self, title="", director="", language="", release_year=""):
         pass
