9ce8ea15f2e25c67d1b2021fc26be5b758b0fae8
Kevin Chung <chungk90@hotmail.com>
2020-03-31 22:25:44 -0700

    Ability class added

--- Labs/Lab11/pokedex_request.py
+++ Labs/Lab11/pokedex_request.py
@@ -3,6 +3,18 @@ import asyncio
 
 
 class Ability:
+
+    def __init__(self, name: str, id_value: int, generation: str,
+                 effect: str, effect_short: str, pokemon: list):
+        self.name = name
+        self.id_value = id_value
+        self.generation = generation
+        self.effect = effect
+        self.effect_short = effect_short
+        self.pokemon = pokemon
+
+
+class PokedexRequest:
     pass
 
 
