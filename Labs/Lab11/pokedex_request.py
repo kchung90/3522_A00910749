0c97ed8d967ba88ee0fb13697151f093301feed2
Kevin Chung <chungk90@hotmail.com>
2020-03-31 22:26:28 -0700

    process_requests() and get_ability_data() implemented

--- Labs/Lab11/pokedex_request.py
+++ Labs/Lab11/pokedex_request.py
@@ -15,7 +15,24 @@ class Ability:
 
 
 class PokedexRequest:
-    pass
+
+    def __init__(self, url: str):
+        self.url = url
+
+    async def process_requests(self, requests: list):
+        async with aiohttp.ClientSession() as session:
+            list_urls = [self.url.format(req_id) for req_id in requests]
+            coroutines = [self.get_ability_data(my_url, session)
+                          for my_url in list_urls]
+            responses = await asyncio.gather(*coroutines)
+            for response in responses:
+                print(response)
+
+    @classmethod
+    async def get_ability_data(cls, url: str, session: aiohttp.ClientSession):
+        response = await session.request(method="GET", url=url)
+        json_response = await response.json()
+        return json_response
 
 
 def main():
